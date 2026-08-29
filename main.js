0dabcb07d8cb1a6e12398d9e084cd40a245c8263
Nic0lAsJoffr3 <nicolasmuitolegal@gmail.com>
2026-08-29 11:48:54 -0300

    Continuando projeto

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import { Respostas } from "./Respostas.js";
 import { ClasseErro, GerarMensagem } from "./utils.js";
 const Conteudos = document.getElementById("Conteudos");
-let ultimaFoiPergunta = false;
+
 let Mensagens = []
+let MensagensCXPG = []
 let PontuacaoTotal = 0;
+let mensagensDesdePergunta = 0;
+let proximoIntervalo = 0;
 
 const AreaDoConteudo = document.getElementById("AreaDoConteudo");
 const Erros = [
@@ -14,7 +17,9 @@ const Erros = [
 
 let QuantErros = 0;
 
-
+AreaDoConteudo.addEventListener("wheel", (e) => {
+    e.preventDefault();
+}, { passive: false });
 
 
 for (let i = 0; i < 10; i++) {
@@ -26,35 +31,49 @@ AreaDoConteudo.addEventListener("scroll", () => {
     const maximo = AreaDoConteudo.scrollHeight - AreaDoConteudo.clientHeight;
     if (maximo - atual < 500) {
         GerarConteudos();
-        if (Mensagens.length > 15) {
+        if (Mensagens.length > 40) {
             Mensagens.shift().remove();
         }
     }
+
 });
+
 
 
 function GerarConteudos() {
     let pergunta = false;
-
-    if (!ultimaFoiPergunta && Math.random() < 0.7) {
+    if (
+        mensagensDesdePergunta >= proximoIntervalo &&
+        Math.random() < 0.7
+    ) {
         pergunta = true;
     }
-    ultimaFoiPergunta = pergunta;
+
+    if (pergunta) {
+        mensagensDesdePergunta = 0;
+        proximoIntervalo = Math.floor(Math.random() * 3) + 3;
+    }
+    else {
+        mensagensDesdePergunta++;
+    }
 
     let conteudo = "";
+
     if (pergunta) {
-        const perguntaSorteada = Math.round(Math.random() * (Respostas.CXPG.length - 1));
+        const perguntaSorteada =
+            Math.floor(Math.random() * Respostas.CXPG.length);
 
         conteudo = GerarMensagem(
             Respostas.CXPG[perguntaSorteada].html,
             perguntaSorteada,
             false
         );
+
+        MensagensCXPG.push(conteudo);
     }
     else {
         conteudo = GerarMensagem("", -1, true);
     }
-
 
     Mensagens.push(conteudo);
     Conteudos.appendChild(conteudo);
@@ -78,29 +97,54 @@ function MudarInput(i) {
     cxpg3.classList[RespostaCXPG == 3 ? "add" : "remove"]("selected");
 }
 
-    export function ReceberRespostaCXPG(i, dom, dialog) {
-        dom.remove();
-        dialog.close();
+export function ReceberRespostaCXPG(i, dom, dialog) {
+    dom.remove();
+    dialog.close();
+    MensagensCXPG.shift();
 
-        let EstaCorreta = Respostas.CXPG[i].respostaCorreta == RespostaCXPG;
-        if (EstaCorreta) {
-            Respostas.CXPG[i].acertos++;
-            PontuacaoTotal += 100 / (Respostas.CXPG[i].acertos);
-
-        }
-        else {
-            QuantErros++;
-        }
+    let EstaCorreta = Respostas.CXPG[i].respostaCorreta == RespostaCXPG;
+    if (EstaCorreta) {
+        Respostas.CXPG[i].acertos++;
+        PontuacaoTotal += 100 / (Respostas.CXPG[i].acertos);
 
     }
+    else {
+        QuantErros++;
+    }
+
+}
+
+
+// Loop
+const PONTOS = document.getElementById("Pontos");
+let velocidade = 20;
+setInterval(() => {
+    if (MensagensCXPG.length > 0) {
+        if (velocidade < 8) velocidade = 8;
+
+        if (AreaDoConteudo.scrollTop < MensagensCXPG[0].offsetTop - 150) {
+            AreaDoConteudo.scrollTop += velocidade;
+            velocidade -= 0.05;
+        }
+        else if (AreaDoConteudo.scrollTop > MensagensCXPG[0].offsetTop - 100) {
+            AreaDoConteudo.scrollTop -= velocidade;
+            velocidade -= 0.05;
+        }
+        else {
+            velocidade = 8
+        }
+    }
+    else {
+        AreaDoConteudo.scrollTop += 15;
+    }
+    PONTOS.innerText = "Pontos: " + PontuacaoTotal
+}, 10)
 
 // Loop Render
 function Render() {
     ClasseErro(Erros[0], QuantErros >= 1);
     ClasseErro(Erros[1], QuantErros >= 2);
     ClasseErro(Erros[2], QuantErros >= 3);
-
-
 
     requestAnimationFrame(Render);
 }
